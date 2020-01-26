59da829abe5820d968cc1c0ffee85013c04c606a
John Erick Cabrera Ramirez <johncafre@gmail.com>
2020-01-26 13:58:57 -0500

    last changes

---
+++ subir_git.sh
@@ -0,0 +1,13 @@
+git pull overleaf master
+#echo johncafre@gmail.com
+#echo 9a0221220277183457
+#echo johncafre@gmail.com
+#echo 9a0221220277183457
+
+git add --all
+git commit -m "a"
+git push overleaf master
+#echo johncafre@gmail.com
+#echo 9a02212202771834
+#echo johncafre@gmail.com
+#echo 9a0221220277183457
